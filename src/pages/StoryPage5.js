095773104d1512bf66b6faf38da0d33ad12a9b26
Thalles <thalles_cansi@hotmail.com>
2024-10-06 22:53:47 -0300

    Acertando botão do vídeo

--- src/pages/StoryPage5.js
+++ src/pages/StoryPage5.js
@@ -21,6 +21,8 @@ export default function StoryPage2() {
 
     return (
         <div className="story-container5">
+            <div className="button-return-ground3"></div>
+            <div className="button-continue-ground3"></div>
             <div className="video-container">
                 <iframe width="100%" height="100%" src="https://www.youtube.com/embed/xAc13Ko-bt4?autoplay=1&loop=1&playlist=xAc13Ko-bt4&controls=0&modestbranding=1&showinfo=0" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
             </div>
